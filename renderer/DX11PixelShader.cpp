6153189b20a51511b3446482718b2ee48a42f247
Jake Maskiewicz <jakemaskiewicz@gmail.com>
2014-04-10 18:53:47 -0700

    added error check to shaders

--- renderer/DX11PixelShader.cpp
+++ renderer/DX11PixelShader.cpp
@@ -1,8 +1,10 @@
 #include "DX11PixelShader.h"
 
+#include <stdexcept>
 
 DX11PixelShader::DX11PixelShader(char* filename, ID3D11Device* device) : DX11Shader(filename) {
-	device->CreatePixelShader(this->bytecode, this->length, NULL, (ID3D11PixelShader**)&this->shader);
+	if (FAILED(device->CreatePixelShader(this->bytecode, this->length, NULL, (ID3D11PixelShader**)&this->shader)))
+		throw std::runtime_error("Could not create pixel shader");
 }
 
 
--- renderer/DX11VertexShader.cpp
+++ renderer/DX11VertexShader.cpp
@@ -1,8 +1,10 @@
 #include "DX11VertexShader.h"
 
+#include <stdexcept>
 
 DX11VertexShader::DX11VertexShader(char* filename, ID3D11Device* device) : DX11Shader(filename) {
-	device->CreateVertexShader(this->bytecode, this->length, NULL, (ID3D11VertexShader**)&this->shader);
+	if (FAILED(device->CreateVertexShader(this->bytecode, this->length, NULL, (ID3D11VertexShader**)&this->shader)))
+		throw std::runtime_error("Could not create vertex shader");
 }
 
 
