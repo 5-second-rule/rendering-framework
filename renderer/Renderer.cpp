a19ac0d2d86f1ae1da97f9c71e1429adac36a0d2
Jake Maskiewicz <jakemaskiewicz@gmail.com>
2014-04-22 17:09:56 -0700

    missing bracket

--- renderer/Renderer.cpp
+++ renderer/Renderer.cpp
@@ -22,46 +22,47 @@ namespace Transmission {
 #endif
 	}
 
-/*
-Load a model from a file path. Returns false if model failed to load or incorrect filepath.
-- filePath: path to 3D model file
-- vP: pointer to VertexBuffer pointer
-- iP: pointer to IndexBuffer pointer
-*/
-bool Renderer::loadModelFile(char* filePath, VertexBuffer** vP, IndexBuffer** iP) {
-	char* extension;
-	HRESULT result;
+	/*
+	Load a model from a file path. Returns false if model failed to load or incorrect filepath.
+	- filePath: path to 3D model file
+	- vP: pointer to VertexBuffer pointer
+	- iP: pointer to IndexBuffer pointer
+	*/
+	bool Renderer::loadModelFile(char* filePath, VertexBuffer** vP, IndexBuffer** iP) {
+		char* extension;
+		HRESULT result;
 
 		extension = strrchr(filePath, '.');
 
-	// Check extension and used necessary model loader
-	if (strcmp(extension, ".obj") == 0){
-		OBJLoader objLoaded;
-		result = objLoaded.loadOBJFile(filePath, vP, iP, this);
-	}
-	else if (strcmp(extension, ".fbx") == 0){
-		FBXLoader fileLoaded;
-		result = fileLoaded.loadFBXFile(filePath, vP, iP, this);
-	}
-	else {
-		return false;
+		// Check extension and used necessary model loader
+		if (strcmp(extension, ".obj") == 0){
+			OBJLoader objLoaded;
+			result = objLoaded.loadOBJFile(filePath, vP, iP, this);
+		}
+		else if (strcmp(extension, ".fbx") == 0){
+			FBXLoader fileLoaded;
+			result = fileLoaded.loadFBXFile(filePath, vP, iP, this);
+		}
+		else {
+			return false;
+		}
+
+		return result == S_OK;
 	}
 
-	return result == S_OK;
-}
-
-/*
-	Creates a model from a file path. Returns pointer to model or nullptr if model failed to load or incorrect filepath.
+	/*
+		Creates a model from a file path. Returns pointer to model or nullptr if model failed to load or incorrect filepath.
 		- filePath: path to 3D model file
 		- vP: pointer to VertexBuffer pointer
 		- iP: pointer to IndexBuffer pointer
-*/
-Model* Renderer::createModelFromFile(char* filePath, VertexBuffer** vP, IndexBuffer** iP) {
-	// Checks if error occured during model load
-	if (this->loadModelFile(filePath, vP, iP)) {
-		return createModel(*vP, *iP);
-	}
-	else {
-		return nullptr;
+		*/
+	Model* Renderer::createModelFromFile(char* filePath, VertexBuffer** vP, IndexBuffer** iP) {
+		// Checks if error occured during model load
+		if (this->loadModelFile(filePath, vP, iP)) {
+			return createModel(*vP, *iP);
+		}
+		else {
+			return nullptr;
+		}
 	}
 }
