cea6e483b6589b9070eefa8b767c42203debce51
Jake Maskiewicz <jakemaskiewicz@gmail.com>
2014-07-03 19:07:43 -0700

    cleanup

--- renderer/render-framework.h
+++ renderer/render-framework.h
@@ -1,19 +1,23 @@
 #pragma once
-#ifdef WIN32
+
 // disable warning about zero-len
 // exporting STL classes, and str
 #pragma warning( disable : 4200 )
 #pragma warning( disable : 4251 )
 #pragma warning( disable : 4996 )
 #pragma warning( disable : 4067 )
+
+// DLL export macro
 #ifdef _IS_RENDER_FRAMEWORK
 #define FRAMEWORKDLL __declspec(dllexport)
 #else
 #define FRAMEWORKDLL __declspec(dllimport)
 #endif
-#endif
 
-#include "renderers.h"
+// list available renderers
+#define RENDERER_NONE 0
+#define RENDERER_DX 1
+#define RENDERER_GL 2
 
 #ifndef RENDERER
 	#define RENDERER RENDERER_NONE
--- renderer/renderers.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define RENDERER_NONE 0
-#define RENDERER_DX 1
-#define RENDERER_GL 2
